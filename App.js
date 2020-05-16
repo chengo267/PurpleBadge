de8d3522686b9d17572cb98320008cecd1ec682d
chengohari <chengo267@gmail.com>
2020-05-16 13:21:37 +0300

    firebase init

--- App.js
+++ App.js
@@ -7,6 +7,22 @@ import EnterScreen from './src/screens/EnterScreen';
 import StayShopScreen from './src/screens/StayShopScrren';
 import CreateUserScreen from './src/screens/CreateUserScreen';
 import LoginScreen from './src/screens/LoginScreen';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjBluS_61uj00Rtn_VS2iufonnSaPZ1kE",
+  authDomain: "purplebadge-6e593.firebaseapp.com",
+  databaseURL: "https://purplebadge-6e593.firebaseio.com",
+  projectId: "purplebadge-6e593",
+  storageBucket: "purplebadge-6e593.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+database.ref('UsersDetails/' + "test").set({
+  test: "test"
+});
+
 
 const navigator = createStackNavigator(
   {
